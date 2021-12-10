476234517498fb03ff09ac03404e54bcfa806b8c
Forest Gregg <fgregg@datamade.us>
2021-12-10 13:36:28 -0500

    fix trimifiy script

--- scripts/trimify.sql
+++ scripts/trimify.sql
@@ -1,3 +1,3 @@
-select 'select group_concat(''update ' || name || ' set '' || name || '' = trim('' || name || ''), '';
+select 'select group_concat(''update ' || name || ' set '' || name || '' = trim('' || name || '')'', '';
 '') || '';
 '' as sql_to_run from pragma_table_info('''||name||''');' from sqlite_schema;
