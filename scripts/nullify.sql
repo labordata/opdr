5b85e9a360b6579f133f17c89faacda46d286ba9
Forest Gregg <fgregg@datamade.us>
2021-12-10 13:08:33 -0500

    un format nullify

--- scripts/nullify.sql
+++ scripts/nullify.sql
@@ -1,12 +1,3 @@
-SELECT
-    'select group_concat(''update ' || name || ' set '' || name || '' = null where '' || name ||'' = ' ''''''''',
-    '';
-
+select 'select group_concat(''update ' || name || ' set '' || name || '' = null where '' || name ||'' = '''''''''', '';
 '') || '';
-
-'' as sql_to_run from pragma_table_info('''||name||''');
-
-'
-FROM
-    sqlite_schema;
-
+'' as sql_to_run from pragma_table_info('''||name||''');' from sqlite_schema;
